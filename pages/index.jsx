fa16df57d29d44573c35a5d8b2e2816c2a5ab58e
anderson <anderson.d.cardoso92@gmail.com>
2023-07-26 15:32:58 -0500

    improve design

--- pages/index.jsx
+++ pages/index.jsx
@@ -23,6 +23,13 @@ export default function Home() {
         </button>
       </section>
       <section className="relative w-full h-full py-5 flex flex-col justify-center text-lg gap-5 items-center">
+        <svg className="absolute top-0 z-9" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#e98489"
+            fill-opacity="0.3"
+            d="M0,64L40,80C80,96,160,128,240,160C320,192,400,224,480,202.7C560,181,640,107,720,69.3C800,32,880,32,960,64C1040,96,1120,160,1200,181.3C1280,203,1360,181,1400,170.7L1440,160L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"
+          ></path>
+        </svg>
         <div className=" absolute right-0 bottom-0">
           <Image
             src="/about-bg.png"
